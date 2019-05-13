97427b3f79c68c5212414f99885b3125ea182e02
jeanpedron <49164424+jeanpedron@users.noreply.github.com>
2019-05-13 15:25:21 -0300

    Add files via upload

---
+++ projeto_jean/FXMLDocumentController.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto_jean;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javax.swing.*;
+
+/**
+ *
+ * @author 05180206
+ */
+public class FXMLDocumentController extends JFrame implements Initializable {
+
+    @FXML
+    private Label label;
+    @FXML
+    private Button buttoncadastro;
+    @FXML
+    private Button buttonsessao;
+    @FXML
+    private Button buttoneditar;
+    @FXML
+    ImageView fotoinicial;
+    @FXML
+    TextField info, listadenome, buscarnome;
+
+    @FXML
+    private void novocadastro(ActionEvent event) {
+       
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLjanela1.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            Stage stage1 = new Stage();
+
+            stage1.setTitle("Sistema de Pacientes");
+            stage1.setScene(scene);
+            stage1.show();
+        } catch (IOException ex) {
+            System.err.println("Erro ao abrir Janela 1");
+        }
+    }
+
+    @FXML
+    private void iniciarsessao(ActionEvent event){
+        //Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //stage.close();
+        //ou 
+        // button1.getScene().getWindow().hide();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLjanela2.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            Stage stage1 = new Stage();
+            
+            stage1.setTitle("Janela Secundária");
+            stage1.setScene(scene);
+            stage1.show();
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+
+    @FXML
+    private void editardados(ActionEvent event) {
+        //pegar arquivo ja aberto
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLjanela1.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            Stage stage1 = new Stage();
+
+            stage1.setTitle("Janela Secundária");
+            stage1.setScene(scene);
+            stage1.show();
+        } catch (IOException ex) {
+            System.err.println("Erro ao abrir Janela 1");
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+}
---
+++ projeto_jean/FXMLjanela2Controller.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto_jean;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.TextFlow;
+
+/**
+ * FXML Controller class
+ *
+ * @author 05180206
+ */
+public class FXMLjanela2Controller implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    TextFlow infojanela2, tempo;
+     @FXML
+    ImageView imgjanela2;
+     @FXML
+    ProgressBar barradeprogresso;
+    
+     
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
